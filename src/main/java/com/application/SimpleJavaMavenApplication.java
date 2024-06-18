0e70cd113e605fe59462600ae7847dd8ab3b8cae
Hemant Rajpoot <hemantrajpoot56@gmail.com>
2024-06-18 23:40:33 +0530

    second commit

---
+++ src/main/java/com/application/SimpleJavaMavenApplication.java
@@ -0,0 +1,13 @@
+package com.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleJavaMavenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleJavaMavenApplication.class, args);
+	}
+
+}
